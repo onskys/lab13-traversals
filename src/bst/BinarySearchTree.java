7922782d6190363cdba88a23985ce74d3783cc92
onskys <owen@Owens-MacBook-Air.local>
2022-05-06 15:00:51 -0700

    utc

--- src/bst/BinarySearchTree.java
+++ src/bst/BinarySearchTree.java
@@ -141,11 +141,23 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	}
 	
 	public void inOrderRecurse(BSTNode<T> node) {
+		if (node == null) {
+			return;
+		}
+		inOrderRecurse(node.leftChild);
+		System.out.print(node.data + " ");
+		inOrderRecurse(node.rightChild);
 		
 	}
+	
 	//Traverse the tree in an inorder fashion but using a stack
 	public void inOrderStack() {
+		if (root == null) {
+			return;
+		}
+		
 		Stack<BSTNode<T>> in = new Stack<BSTNode<T>>();
+		
 		
 		
 	}
